01fc767993c139228c4d6e85a1ee79fa59ddba7a
Daniel J. Finnegan <djf32@bath.ac.uk>
2018-06-08 11:53:39 +0100

    Updated find utility for unity

--- Urban-Planner/cmake/FindUnityGame.cmake
+++ Urban-Planner/cmake/FindUnityGame.cmake
@@ -5,7 +5,6 @@
 #
 # Variables:
 # 	- UnityGame_EXECUTABLE -> The main Unity executable
-#	- UnityGame_VERSION_STRING -> The version number of Unity
 #
 #=============================================================================
 # Copyright 2018 Daniel J. Finnegan
@@ -20,11 +19,13 @@
 # (To distribute this file outside of CMake, substitute the full
 #  License text for the above reference.)
 
+include (
+	FindPackageHandleStandardArgs
+)
+
 # Search the windows registry for the installation of Unity
 
 if (WIN32)
-
-if (NOT UnityGame_FOUND)
 
 	get_filename_component (
 		UnityGame_EXECUTABLE
@@ -41,29 +42,6 @@ if (NOT UnityGame_FOUND)
 
 	# Do a match on Unity to make sure we found the right key
 	if (UnityGame_EXECUTABLE MATCHES "[Uu]nity")
-
-		set (
-			UnityGame_FOUND
-			true
-			CACHE
-			BOOL
-			""
-		)
-
-	else ()
-
-		set (
-			UnityGame_FOUND
-			false
-			CACHE
-			BOOL
-			""
-		)
-
-	endif ()
-
-	# If we found Unity, go generate the version string
-	if (UnityGame_FOUND)
 
 		string(
 			REPLACE
@@ -96,87 +74,31 @@ if (NOT UnityGame_FOUND)
 		)
 
 		set (
-			UnityGame_VERSION_STRING
+			UnityGame_VERSION
 			${UnityGame_VERSION_MAJOR}.${UnityGame_VERSION_MINOR}
 			CACHE
 			STRING
 			"The version of unity installed on the system"
 		)
 
-		if(UnityGame_FIND_VERSION_EXACT)
-
-			if (NOT ${UnityGame_VERSION_STRING} VERSION_EQUAL ${UnityGame_FIND_VERSION})
-
-				set (
-					UnityGame_FOUND
-					false
-					CACHE
-					BOOL
-					""
-					FORCE
-				)
-
-				message (
-					STATUS
-					"Requested Unity version ${UnityGame_FIND_VERSION} but found ${UnityGame_VERSION_STRING}"
-				)
-
-			else ()
-			
-				set (
-					UnityGame_FOUND
-					true
-					CACHE
-					BOOL
-					""
-					FORCE
-				)
-
-			endif ()
-
-		endif ()
-
-		if (${UnityGame_VERSION_STRING} VERSION_LESS ${UnityGame_FIND_VERSION})
-
-			set (
-				UnityGame_FOUND
-				false
-				CACHE
-				BOOL
-				""
-				FORCE
-			)
-
-			message (
-				STATUS
-				"Requested Unity version ${UnityGame_FIND_VERSION} but found ${UnityGame_VERSION_STRING}"
-			)
-
-		endif ()
 
 	endif ()
 
-	if (UnityGame_FOUND)
-
-	message (
-		STATUS
-		"Found unity : ${UnityGame_EXECUTABLE}"
+	# One doesn't need to even check for validity
+	# on the UnityGame_EXECUTABLE as this function
+	# will handle the validity check
+	find_package_handle_standard_args (
+		UnityGame
+			UnityGame_VERSION
+		FAIL_MESSAGE
+			"Could not find a suitable installation of Unity3D"
 	)
-
-	message (
-		STATUS
-		"Unity version : ${UnityGame_VERSION_STRING}"
-	)	
-
-	endif ()
-
-endif ()
 
 else ()
 
-message (
-	FATAL_ERROR
-	"Only Windows 64-Bit supported!"
-)
+	message (
+		FATAL_ERROR
+		"Only Windows 64-Bit supported!"
+	)
 
 endif ()
