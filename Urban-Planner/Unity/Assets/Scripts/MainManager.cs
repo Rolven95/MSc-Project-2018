741e7ebf92857040b23aa532222c5ef7e60ede34
Ruowen <rc864@bath.ac.uk>
2018-07-24 16:28:11 +0100

    Multipul Object tracking & decoder bug fixed
    tracking in new scene; decoder col row wrong, fixed now, still need new code mapping table.

--- Urban-Planner/Unity/Assets/Scripts/MainManager.cs
+++ Urban-Planner/Unity/Assets/Scripts/MainManager.cs
@@ -4,84 +4,45 @@ using UnityEngine;
 using System.Runtime.InteropServices;
 using System;
 
-//struct ObjectInfomation
-//{
-//     int id;
-//     int x;
-//     int y;
-//     int z;
-//     int r;
-//};
-
 public class MainManager : MonoBehaviour
 {
-    public const int TotalNumber = 1;
-    public int TemCurrentDegree = 0; 
+    public const int TotalNumber = 3;
 
+    public int[] TemCurrentDegree;
+    
 
     public GameObject[] Object_List = new GameObject[TotalNumber]; 
-    //// 0-4
-    //public GameObject Rock0;//0
-    //public GameObject Rock1;
-    //public GameObject Rock2;
-    //public GameObject Rock3;
-    //public GameObject Rock4;//4
-    ////5-9
-    //public GameObject Tree0;//5
-    //public GameObject Tree1;
-    //public GameObject Tree2;
-    //public GameObject Tree3;
-    //public GameObject Tree4;//9
-
-    //public GameObject lamppost0;//10
-    //public GameObject lamppost1;
-    //public GameObject lamppost2;
-    //public GameObject lamppost3;//13
-
-    //public GameObject bench0;//14
-    //public GameObject bench1;
-    //public GameObject bench2;
-    //public GameObject bench3;
-    //public GameObject bench4;//18
- 
     int[][] allinfo = new int[30][]; // [0]id,[1]x,[2]y,[3]z,[4]rotation, [5]lifetime 
    
-        
-
-    float [][] CurrentLocation = new float[30][]; //x,y,z,r
-
-    //ObjectInfomation[] AllCurrentLocation = new ObjectInfomation[TotalNumber];
+    float [][] CurrentLocation = new float[TotalNumber][]; //x,y,z,r
 
     public Space rotateSpace;
 
     void Start()
     {
         //this should be where load all location into the CurrentLocation[][]. Use CurrentLocation as a mapping table. 
-
-        for (int i =0; i< TotalNumber; i++) {
+        TemCurrentDegree = new int[TotalNumber];
+        
+        Debug.Log("CurrentLength(3)="+ CurrentLocation.Length);
+        for (int i =0; i< CurrentLocation.Length; i++) {
+            
             CurrentLocation[i] = new float[4];
-            allinfo[i] = new int[6];
-           
+
             CurrentLocation[i][0]= Object_List[i].transform.position.x;
             CurrentLocation[i][1]= Object_List[i].transform.position.y;
             CurrentLocation[i][2]= Object_List[i].transform.position.z;
             CurrentLocation[i][3]= 0; // differ of current  and reading angle 
 
+        }
+        for (int i = 0; i < allinfo.Length; i++)
+        {
+            allinfo[i] = new int[6];
             allinfo[i][0] = 99;
             allinfo[i][1] = 0;
             allinfo[i][2] = 0;
             allinfo[i][3] = 0;
             allinfo[i][4] = 0;
-
         }
-
-        //for (int i = TotalNumber; i < allinfo.Length; i++) {
-        //    allinfo[i][0] = 99;
-        //    allinfo[i][1] = 0;
-        //    allinfo[i][2] = 0;
-        //    allinfo[i][3] = 0;
-        //    allinfo[i][4] = 0;
-        //}
     }
 
     // Update is called once per frame
@@ -100,9 +61,7 @@ public class MainManager : MonoBehaviour
     int[,] qrresult = new int[30, 6];
 
     public void QR_Reader(int[][] newdata) { // get image and read QR codes. Put infomation in the allinfo[][]
-
-
-
+        
         //Update the sensor and underlying
         QRAPI.updateSensor();
         QRAPI.updateProcessor();
@@ -112,22 +71,21 @@ public class MainManager : MonoBehaviour
         {
             for (int i = 0; i < TotalNumber; i++)
             {
-                newdata[i][0] = 0;
-                if (qrresult[i, 0] != 99 && qrresult[i, 0] >= 0)
+                if(qrresult[i, 0] != 99)
+                 Debug.Log("ID:" + qrresult[i, 0]);
+
+                if (qrresult[i, 0] != 99 && Check_range(qrresult[i, 0]))
                 {
-                    // newdata[0] = 0;
+                    newdata[i][0] = qrresult[i, 0];
                     newdata[i][1] = -1 * (qrresult[i, 1] * 2200 / 12800 - 110);
                     newdata[i][2] = 0;
                     newdata[i][3] = -1*(qrresult[i, 2]*2200/ 7200 - 110);
                     newdata[i][4] = qrresult[i, 4];
 
-                    Debug.Log(" "+newdata[i][1] + " " + newdata[i][2] + " " + newdata[i][3] + " " + newdata[i][4]);
+                    Debug.Log("ID:"+newdata[i][0]+" " +newdata[i][1] + " " + newdata[i][2] + " " + newdata[i][3] + " " + newdata[i][4]);
                 }
             }
         }
-
-
-        
         return; 
     }
 
@@ -135,25 +93,23 @@ public class MainManager : MonoBehaviour
         Debug.Log("entered Location_Updater");
         for (int i = 0; i < TotalNumber; i++)
         {
-
-            Debug.Log("QRinfo[i][0]: " + QRinfo[i][0]);
+            
             if (QRinfo[i][0] != 99)
             {
+                Debug.Log("QRinfo[i][0]: " + QRinfo[i][0]);
+                Debug.Log("i=" + i);
                 Current[QRinfo[i][0]][0] = QRinfo[i][1];
                 Current[QRinfo[i][0]][1] = QRinfo[i][2];
                 Current[QRinfo[i][0]][2] = QRinfo[i][3];
+                Current[QRinfo[i][0]][3] = QRinfo[i][4] - TemCurrentDegree[i];
 
-                Current[QRinfo[i][0]][3] = QRinfo[i][4]- TemCurrentDegree;
-
-                TemCurrentDegree = QRinfo[i][4];
-
-                //Debug.Log("Updated");
+                TemCurrentDegree[i] = QRinfo[i][4];
+                Debug.Log("Updated");
             }
-            //Debug.Log("Checked AllInfolist");
+            Debug.Log("Checked AllInfolist");
         }
         return;
     }
-
      
     public void Object_Mover(float[][] Current){ //use new locations to move objects  [id][x,y,z,rotation]
 
@@ -161,36 +117,20 @@ public class MainManager : MonoBehaviour
             change_position(Current[i], Object_List[i]);
             Debug.Log("Moved");
         }
-
         return;
     }
-
-
-    public bool Check_List(int id) {
-        if (id > 0 && id < TotalNumber)
-            return true;
-        else
-            return false; 
-    }
-
-
-
     void change_position(float[] Current, GameObject Objects) // 
     {
-            Objects.transform.position = new Vector3(Objects.transform.position.x /2 +  Current[0]/2, Objects.transform.position.y / 2+  Current[1]/2, Objects.transform.position.z /2 + Current[2]/2);
-
-       // if (Objects.transform.rotation.y != Current[3]/180)
-           // Objects.transform.Rotate(0, (Current[3]/180  - Objects.transform.rotation.y), 0, Space.World);
-
-        //Objects.transform.Rotate(0, ((Current[3]-180)/180-Objects.transform.rotation.y), 0, Space.World);
-        // Debug.Log("Current[3]:" + Current[3] + "Current3/180=" +( Current[3] / 180 )+ "y=" + Objects.transform.rotation.y );
-
-       // Debug.Log("Objects.transform.rotation.y:"+ Objects.transform.rotation.y);
+        Objects.transform.position = new Vector3(Objects.transform.position.x /2 +  Current[0]/2, Objects.transform.position.y / 2+  Current[1]/2, Objects.transform.position.z /2 + Current[2]/2);
         Objects.transform.Rotate(new Vector3(0, Current[3], 0), rotateSpace);
-
         //Debug.Log("position changed");
     }
+    bool Check_range(int id) {
+        if (id>=0 && id <TotalNumber) 
+            return true;
 
+        return false;
+    }
     private static class QRAPI
     {
         [DllImport("HandTrackerLib")]
